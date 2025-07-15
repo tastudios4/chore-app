7282eab79e5d8601833ba29b9b9087a2a4a721d8
tastudios4 <thienanstudios@gmail.com>
2025-07-15 15:20:47 -0700

    Initial project setup with Spring Boot backend and database config

---
+++ chore-system-backend/src/main/java/com/mychoreapp/chore_system_backend/ChoreSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mychoreapp.chore_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoreSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoreSystemBackendApplication.class, args);
+	}
+
+}
---
+++ chore-system-backend/src/test/java/com/mychoreapp/chore_system_backend/ChoreSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mychoreapp.chore_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoreSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
